7af10ea09cbf781d79d6a55a28925c52bafbeb98
John Jun <tbvjdngus@gmail.com>
2024-02-15 17:57:36 +0900

    "Login API connection made"

--- front/src/app/_component/MemberRecoilState.ts
+++ front/src/app/_component/MemberRecoilState.ts
@@ -3,12 +3,9 @@ import { atom } from "recoil";
 import { recoilPersist } from 'recoil-persist';
 
 
-type CustomStorage = Pick<Storage, 'getItem' | 'setItem' | 'removeItem'>;
-const storage = window.localStorage as CustomStorage;
-
 const { persistAtom } = recoilPersist({
     key: 'recoil-persist', // this key is used to store data in localStorage
-    storage: storage, // configure the storage mechanism
+    storage: localStorage, // configure the storage mechanism
 });
 
 export const Member = atom({
