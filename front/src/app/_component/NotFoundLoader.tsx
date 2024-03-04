2d47b46f5afb02c39910112a98211082329ef7c3
John Jun <tbvjdngus@gmail.com>
2024-03-04 13:16:31 +0900

    "notFoundPage edited"

---
+++ front/src/app/_component/NotFoundLoader.tsx
@@ -0,0 +1,33 @@
+type Props={
+    width: string,
+    height:string
+}
+export default function NotFoundLoader({width, height}: Props){
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" id="epahIlPfzVj1" width={width} height={height} viewBox="0 0 1920 1080" shape-rendering="geometricPrecision" text-rendering="geometricPrecision">
+            <g transform="translate(.000001 0)">
+                <g transform="translate(.000001 0.000001)">
+                    <rect width="80.665978" height="405.346542" rx="26" ry="26" transform="matrix(.861893 0.50709-.50709 0.861893 536.864732 199.008335)" fill="#9fd3c7" stroke-width="0"/>
+                    <rect width="80.665978" height="405.346542" rx="26" ry="26" transform="translate(608.616376 386.161876)" fill="#9fd3c7" stroke-width="0"/>
+                    <rect width="80.665978" height="405.346542" rx="26" ry="26" transform="matrix(-.000011 1-1-.000011 771.427794 499.669328)" fill="#9fd3c7" stroke-width="0"/>
+                    </g><g transform="matrix(-1 0 0 1 1929.57164-.887192)">
+                        <rect width="80.665978" height="405.346542" rx="26" ry="26" transform="matrix(.861893 0.50709-.50709 0.861893 540.864732 199.008335)" fill="#9fd3c7" stroke-width="0"/>
+                        <rect width="80.665978" height="405.346542" rx="26" ry="26" transform="translate(612.616376 386.161876)" fill="#9fd3c7" stroke-width="0"/>
+                        <rect width="80.665978" height="405.346542" rx="26" ry="26" transform="matrix(-.000011 1-1-.000011 775.427794 499.669328)" fill="#9fd3c7" stroke-width="0"/>
+                        </g>
+                        <ellipse rx="209.731544" ry="291.405846" transform="matrix(1.08846 0 0 1 960 510.866632)" fill="#9fd3c7" stroke-width="0"/>
+                        <g transform="translate(-8.115631 8.000001)">
+                            <rect width="80.665978" height="405.346542" rx="26" ry="26" transform="matrix(1 0 0 1.291715 925.554037 278.25259)" fill="#fff" stroke-width="0"/>
+                            <rect width="80.665978" height="405.346542" rx="26" ry="26" transform="matrix(-.000011 1-.752783-.000009 1118.45648 450.882302)" fill="#fff" stroke-width="0"/>
+                            </g>
+                            <g transform="translate(-319.753743 66.761974)">
+                                <rect width="80.665978" height="405.346542" rx="26" ry="26" transform="matrix(1 0 0-.999796 915.470789 743.082827)" fill="#fff" stroke-width="0"/>
+                                <rect width="80.665978" height="405.346542" rx="26" ry="26" transform="matrix(-.000011 1-.752783-.000009 1110.601862 451.284509)" fill="#fff" stroke-width="0"/>
+                                </g>
+                                <g transform="translate(331.624045 66.761974)">
+                                    <rect width="80.665978" height="405.346542" rx="26" ry="26" transform="matrix(1 0 0-.999796 915.470789 743.082827)" fill="#fff" stroke-width="0"/>
+                                    <rect width="80.665978" height="405.346542" rx="26" ry="26" transform="matrix(-.000011 1-.752783-.000009 1110.601862 451.284509)" fill="#fff" stroke-width="0"/>
+                                    </g></g></svg>
+    )
+}
--- front/src/app/not-found.tsx
+++ front/src/app/not-found.tsx
@@ -1,13 +1,37 @@
-import Link from "next/link";
+"use client";
 import {NextPage} from "next";
+import NotFoundLoader from "./_component/NotFoundLoader";
+import style from './_component/notFoundLoader.module.css'
+import {FormEventHandler} from "react";
+import {useRouter} from "next/navigation";
+
 
 const NotFound: NextPage = ()=> {
+    const router = useRouter();
+    const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+        router.push(`/search?q=${event.currentTarget.search.value}`);
+    }
 return (
-
-<div>
-page is not found. Search other page
-<Link href= "/seach"> search </Link>
+    
+<body className={style.Publications}>
+<div className={style.main}>
+<NotFoundLoader width="400px" height="350px"/>
+<h4>요청하신 페이지가 존재하지 않습니다.<br/>다시한번 검색해 보세요.</h4>
+<div className={style.searchBox}>
+<form className={style.search} onSubmit={onSubmit}>
+            <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
+                <g>
+                    <path
+                        d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
+                </g>
+            </svg>
+            <input type="search" name="search" />
+        </form>
 </div>
+
+</div>
+</body>
 )
 
 }
