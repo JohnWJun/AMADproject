51e09f8d460599c3bbd18d203c34f278f04d58fe
SEB_BE_45_전우현 <tbvjdngus@gmail.com>
2026-02-27 08:52:18 +0900

    "Compose AMAD Modal CSS Fix"

--- front/src/app/(afterLogin)/_component/ComposeAmad.tsx
+++ front/src/app/(afterLogin)/_component/ComposeAmad.tsx
@@ -168,15 +168,17 @@ export default function ComposeAmad() {
     return (
         <div className={style.modalBackground}>
             <div className={style.modal}>
-                <button className={style.closeButton} onClick={onClickClose}>
-                    <svg width={24} viewBox="0 0 24 24" aria-hidden="true"
-                         className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03">
-                        <g>
-                            <path
-                                d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
-                        </g>
-                    </svg>
-                </button>
+                <div className={style.modalHeader}>
+                    <button className={style.closeButton} onClick={onClickClose}>
+                        <svg width={24} viewBox="0 0 24 24" aria-hidden="true"
+                             className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03">
+                            <g>
+                                <path
+                                    d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
+                            </g>
+                        </svg>
+                    </button>
+                </div>
                 <div className={style.innerModal}>
                 <form className={style.modalForm} onSubmit={onSubmit}>
                     <div className={style.modalBody}>
